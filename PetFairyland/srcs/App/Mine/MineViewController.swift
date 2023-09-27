5af49b24758488b8c1d647686f15d9986b9344bb
Ja <jinan@junying-tech.com>
2023-09-27 16:58:12 +0800

    change file path

--- PetFairyland/srcs/App/Mine/MineViewController.swift
+++ PetFairyland/srcs/App/Mine/MineViewController.swift
@@ -6,10 +6,33 @@
 //
 
 import UIKit
+import FDFullscreenPopGesture
 
 class MineViewController: PFBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "我的"
+        
+        setupUI()
+    }
+}
+
+private extension MineViewController {
+    @objc
+    func changeSwitchValue(sw: UISwitch) {
+        PFAccount.shared.isLogin = sw.isOn
+        view.setNeedsUpdateConstraints()
+    }
+}
+
+extension MineViewController {
+    func setupUI() {
+        fd_prefersNavigationBarHidden = true
+        
+        let loginSwitch = UISwitch(frame: .zero)
+        loginSwitch.sizeToFit()
+        loginSwitch.isOn = false
+        loginSwitch.addTarget(self, action: #selector(changeSwitchValue), for: .valueChanged)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: loginSwitch)
     }
 }
